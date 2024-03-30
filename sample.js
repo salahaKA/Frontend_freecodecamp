5883e5c698d22789bf203789e5b4a7ce7493bc5d
Kadeejath Salaha <codestudy475@gmail.com>
2024-03-30 06:34:52 +0530

    first commit

---
+++ sample.js
@@ -0,0 +1,90 @@
+const myArr= [10,20,30,40]
+myArr.push(50)
+
+console.log(myArr)
+
+console.log(myArr.reverse())
+
+
+console.log("-----------------------------------------------")
+
+const firstVar=20
+console.log("first var:",firstVar)
+
+console.log("-----------------------------------------------")
+
+const v1= "Learning to code"
+
+const v2={firstProperty:"hello world"};
+console.log(v1)
+console.log(v2)
+
+console.log("-----------------------------------------------")
+
+const v3= 'helloworld';
+let v4;
+v4=20;
+console.log(v4)
+console.log("-----------------------------------------------")
+
+const var1= 10;
+let var2= 10;
+var var3= 10;
+var var3= 20;
+var2+=1
+console.log(var2)
+console.log("-----------------------------------------------")
+
+const a=10,b=20;
+console.log(a+b)
+
+const var4= {
+    varType:'object',
+    varValue: "some value"
+}
+console.log(var4)
+console.log(typeof var4);
+console.log("-----------------------------------------------")
+// dtypes in js
+var sname='salaha'
+console.log(typeof sname)
+console.log(sname+b)
+console.log(sname,b)
+console.log("-----------------------------------------------")
+const arr1=[10,20,30]
+const arr2=[10,'string',{fname:'kdeejath'},[1,2]];
+console.log(typeof arr1)
+console.log(typeof arr2)
+console.log(arr1[2])
+console.log(arr2[2])
+console.log(arr2[3])
+console.log(arr2[3][0])
+console.log(arr2[2].fname)
+console.log("-----------------------------------------------")
+// object var
+const obj1= {rno:21, fname:'kadee',class:'mca'}
+console.log(obj1.class)
+
+const nestedobj= {
+    layer1:{
+        layer2:{
+            layer3:{
+                targetValue:20
+            }
+        }
+    }
+}
+console.log(nestedobj)
+console.log(nestedobj.layer1.layer2.layer3.targetValue)
+console.log(typeof nestedobj.layer1.layer2.layer3.targetValue)
+
+console.log("-----------------------------------------------")
+
+// functions
+var add= function(){
+    return 10+20;
+}
+console.log(add())
+
+console.log("-----------------------------------------------")
+// js opearator
